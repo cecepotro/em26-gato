7be3c599303c8cf434d975e29be2b9b83b9bf28a
cecepotro <jesus.perez9726@potros.itson.edu.mx>
2026-02-09 20:46:53 -0700

    Project created

--- persistence/db.py
+++ persistence/db.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="65.99.225.32",
+        host="localhost",
         user="siste214_gato_si",
         password = "ITSON@2026#si",
         database="siste214_gato_si"
